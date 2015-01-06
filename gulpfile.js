bf4b432b88aa04a3c6199d20a30153de3ec43e30
Thibaut Dutartre <t.dutartre@gmail.com>
2015-01-06 20:54:09 +0100

    Livereload & gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-var gulp      = require('gulp'),
-    size      = require('gulp-size'),
-    less      = require('gulp-less'),
-    prefix    = require('gulp-autoprefixer'),
-    rename    = require('gulp-rename'),
-    minifyCSS = require('gulp-minify-css'),
-    mozjpeg   = require('imagemin-mozjpeg');
+var gulp       = require('gulp'),
+    size       = require('gulp-size'),
+    less       = require('gulp-less'),
+    prefix     = require('gulp-autoprefixer'),
+    rename     = require('gulp-rename'),
+    minifyCSS  = require('gulp-minify-css'),
+    mozjpeg    = require('imagemin-mozjpeg'),
+    livereload = require('gulp-livereload');
 
 
 gulp.task('minify-images', function () {
@@ -12,7 +13,8 @@ gulp.task('minify-images', function () {
     .pipe(size({gzip: true, title: 'src'}))
     .pipe(mozjpeg({ quality: '65-80' })())
     .pipe(size({gzip: true, title: 'minified'}))
-    .pipe(gulp.dest('images'));
+    .pipe(gulp.dest('images'))
+    .pipe(livereload());
 });
 
 gulp.task('compile-style', function() {
@@ -20,13 +22,21 @@ gulp.task('compile-style', function() {
     .pipe(less())
     .pipe(prefix('last 2 version', 'ie >= 7'))
     .pipe(size({gzip: true, showFiles: true}))
-    .pipe(gulp.dest('css'));
-});
-
-gulp.task('minify-css', function() {
-    gulp.src('css/main.css')
+    .pipe(gulp.dest('css'))
     .pipe(minifyCSS())
     .pipe(rename('main.min.css'))
     .pipe(size({gzip: true, showFiles: true}))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe(livereload());
+});
+
+gulp.task('reload-html', function () {
+    gulp.src('*html')
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('src/styles/*.less', ['compile-style']);
+    gulp.watch('*.html', ['reload-html']);
 });
