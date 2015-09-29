f8a0f878252a60e7e0d7ce83c8f8c9ca16b06b7a
Thibaut Dutartre <t.dutartre@gmail.com>
2015-09-29 23:26:21 +0200

    trying again

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  
-git checkout gh-pages
 
 npm run build
 
@@ -9,9 +7,8 @@ git config user.name "Travis CI"
 git config user.email "contact@travis-ci.com"
 
 git add --all
-git status
 git commit -m "Deploy assets from build #$TRAVIS_BUILD_NUMBER"
 
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages
 
 fi
