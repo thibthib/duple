b4f8ff00e409adfbead48c1375beb2a50cd35978
Thibaut Dutartre <t.dutartre@gmail.com>
2015-09-29 23:21:46 +0200

    Trying to debug deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ npm run build
 git config user.name "Travis CI"
 git config user.email "contact@travis-ci.com"
 
-git add .
+git add --all
+git status
 git commit -m "Deploy assets from build #$TRAVIS_BUILD_NUMBER"
 
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages > /dev/null 2>&1
