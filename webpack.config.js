16c56748823f4026551846dc83ee2f0fb466f827
Thibaut Dutartre <t.dutartre@gmail.com>
2015-10-05 22:46:48 +0200

    Reducing assets file name length

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
       { test: /\.json$/, loader: 'json'},
       { test: /\.less$/, loader: 'style!css!less' },
       { test: /\.css$/, loader: 'style!css' },
-      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'file!image-webpack?progressive=true' }
+      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'file?name=[hash:6].[ext]!image-webpack?progressive=true' }
     ]
   },
   resolve: {
