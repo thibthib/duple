97c93fa76b78266760d46108ab07b3e2783eaff8
Thibaut Dutartre <t.dutartre@gmail.com>
2016-03-13 17:02:41 +0100

    Update dependecies

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
   module: {
     loaders: [
 	  { test: /\.html$/, loader: 'html'},
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
       { test: /\.json$/, loader: 'json'},
       { test: /\.less$/, loader: 'style!css!less' },
       { test: /\.css$/, loader: 'style!css' },
